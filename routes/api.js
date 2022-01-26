85abe682d85dc5716781c259b3e9e41aa8828cb6
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-26 10:47:10 +0700

    p

--- routes/api.js
+++ routes/api.js
@@ -875,27 +875,20 @@ res.sendFile(__path + '/views/apikey-not-found.html');
 }
 })
 
-router.get('/darkjokes', async (req, res, next) => {
-        var Apikey = req.query.apikey
-            
-	if(!Apikey) return res.json(loghandler.notparam)
-	if(listkey.includes(Apikey)){
-
-       fetch(encodeURI(`https://privatasw.herokuapp.com/darkjokes`))
-        .then(response => response.json())
-        .then(data => {
-        var result = data.result;
-             res.json({
-                 creator : `${creator}`,
-                 result
-             })
-         })
-         .catch(e => {
-         	res.json(loghandler.error)
-})
-} else {
-res.sendFile(__path + '/views/apikey-not-found.html');
-}
+router.get("/darkjokes", async (req, res, next) => {
+  
+  apikey = req.query.apikey;
+  
+  if(!apikey) return res.json(loghandler.notparam)
+  
+  if(listkey.includes(apikey)) {
+    let hasil = 'https://api.lolhuman.xyz/api/meme/darkjoke?apikey=rey2k21' 
+    data = await fetch(hasil).then(v => v.buffer())
+    await fs.writeFileSync(__path +'/tmp/darkjokes.jpeg', data)
+    res.sendFile(__path +'/tmp/darkjokes.jpeg')
+  } else {
+    res.sendFile(__path + '/views/apikey-not-found.html');
+  }
 })
 
 router.get("/loli", async (req, res, next) => {
