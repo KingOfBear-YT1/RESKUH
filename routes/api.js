d0018c761ec4db0360f82a61673d4b68163f4567
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-31 21:27:31 +0700

    p

--- routes/api.js
+++ routes/api.js
@@ -936,7 +936,7 @@ router.get('/textpro/welcome', async (req, res, next) => {
   img2= req.query.img2;
   background= req.query.background;
   if(!apikey) return res.json(loghandler.notparam)
-  if(!text) return res.json(loghandler.notquery)
+  
   
   if(listkey.includes(apikey)){
 fetch(encodeURI(`https://api.lolhuman.xyz/api/base/welcomeimage?apikey=rey2k21&img1=${img1}&img2=${img2}&background=${background}&username=${text}&member=${text2}&groupname=${text3}`))
