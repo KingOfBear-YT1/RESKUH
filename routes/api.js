cb01c3a1d7c3cd3cabf3fa1d9f8f937f05894b7b
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-03-01 19:04:40 +0700

    p

--- routes/api.js
+++ routes/api.js
@@ -772,6 +772,34 @@ router.get('/download/pornhub', async(req, res, next) => {
              res.json({
                  status : true,
                  creator : `${creator}`,
+		 result
+             })
+         })
+         .catch(e => {
+         	res.json(loghandler.error)
+})
+} else {
+  res.json(loghandler.invalidKey)
+}
+})
+
+router.get('/download/xvideo', async(req, res, next) => {
+  const apikey = req.query.apikey;
+  const query = req.query.query;
+  
+  if(!query) return res.json(loghandler.notquery)
+  if(!apikey) return res.json(loghandler.notparam)
+  
+  if(listkey.includes(apikey)){
+    fetch(encodeURI(`http://kocakz.herokuapp.com/api/media/xvideo/search?query=${query}`))
+    .then(response => response.json())
+        .then(hasil => {
+
+        var result = hasil.result;
+             res.json({
+                 status : true,
+                 creator : `${creator}`,
+                 result
              })
          })
          .catch(e => {
