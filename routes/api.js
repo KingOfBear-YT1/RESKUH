9c178a026a4355799a1587927150b19b2d5099cc
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-03-01 17:45:51 +0700

    Update

--- routes/api.js
+++ routes/api.js
@@ -2497,6 +2497,29 @@ router.get('/kuis/caklontong', async (req, res, next) => {
     }
 })
 
+router.get('/kuis/tekateki', async (req, res, next) => {
+    var Apikey = req.query.apikey
+
+    if(!Apikey) return res.json(loghandler.notparam)
+    if(listkey.includes(Apikey)){
+        var soal = JSON.parse(
+            fs.readFileSync(__path + '/data/tekateki.json')
+        )
+	res
+          .status(200)
+          .json({
+              code: 200,
+              success: true,
+	      creator: `${creator}`,
+	      result: {
+             	...soal[~~(Math.random() * soal.length)] 
+	      }
+          })
+    } else {
+        res.json(loghandler.invalidKey)
+    }
+})
+
 router.get('/kuis/tebakbendera', async (req, res, next) => {
         var Apikey = req.query.apikey
 	if(!Apikey) return res.json(loghandler.notparam)
