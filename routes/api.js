3f983ec42376909dabe94e150d4df014002324e4
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-03-01 17:20:49 +0700

    p

--- routes/api.js
+++ routes/api.js
@@ -2574,6 +2574,29 @@ router.get('/kuis/asahotak', async (req, res, next) => {
     }
 })
 
+router.get('/kuis/tebaktebakan', async (req, res, next) => {
+    var Apikey = req.query.apikey
+
+    if(!Apikey) return res.json(loghandler.notparam)
+    if(listkey.includes(Apikey)){
+        var soal = JSON.parse(
+            fs.readFileSync(__path + '/data/tebaktebakan.json')
+        )
+	res
+          .status(200)
+          .json({
+              code: 200,
+              success: true,
+	      creator: `${creator}`,
+	      result: {
+             	...soal[~~(Math.random() * soal.length)] 
+	      }
+          })
+    } else {
+        res.json(loghandler.invalidKey)
+    }
+})
+
 router.get('/kuis/family100', async (req, res, next) => {
     var Apikey = req.query.apikey
 
